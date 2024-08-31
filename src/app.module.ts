7811ec3cf351bd1a0df38d9eeff44d42cb1da2b7
hlfr07 <hf46.78@gmail.com>
2024-08-31 18:01:02 -0500

    se agrego los variables globales para trajar seguros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PerfilesModule } from './perfiles/perfiles.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: process.env.POSTGRES_SSL === 'true' ? { rejectUnauthorized: false } : null,
+    }),
+    PerfilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
