de51c82f2deade934e152bd68bda8ec14a14a353
lolijorddy <lolijorddy@gmail.com>
2024-09-07 12:03:57 -0500

    se implemento la documentacion de la api en la tabla permiso

--- src/permisos/dto/create-permiso.dto.ts
+++ src/permisos/dto/create-permiso.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
 import { IsNotEmpty, IsString, MaxLength, MinLength} from "class-validator";
 
 export class CreatePermisoDto {
+    @ApiProperty()
     @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
     @IsNotEmpty({ message: 'El permiso get no puede estar vacío' })
     @IsString({ message: 'El permiso get debe ser un texto' })
@@ -9,6 +11,7 @@ export class CreatePermisoDto {
     @MinLength(3, { message: 'El permiso get debe tener más de 3 caracteres' })
     get: string;
 
+    @ApiProperty()
     @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
     @IsNotEmpty({ message: 'El permiso post no puede estar vacío' })
     @IsString({ message: 'El permiso post debe ser un texto' })
@@ -16,6 +19,7 @@ export class CreatePermisoDto {
     @MinLength(3, { message: 'El permiso post debe tener más de 3 caracteres' })
     post: string;
 
+    @ApiProperty()
     @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
     @IsNotEmpty({ message: 'El permiso put no puede estar vacío' })
     @IsString({ message: 'El permiso put debe ser un texto' })
@@ -23,6 +27,7 @@ export class CreatePermisoDto {
     @MinLength(3, { message: 'El permiso put debe tener más de 3 caracteres' })
     put: string;
 
+    @ApiProperty()
     @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
     @IsNotEmpty({ message: 'El permiso delete no puede estar vacío' })
     @IsString({ message: 'El permiso delete debe ser un texto' })
--- src/permisos/permisos.controller.ts
+++ src/permisos/permisos.controller.ts
@@ -2,26 +2,32 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PermisosService } from './permisos.service';
 import { CreatePermisoDto } from './dto/create-permiso.dto';
 import { UpdatePermisoDto } from './dto/update-permiso.dto';
-
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { get } from 'http';
+import { GetPermisoDto } from './dto/get-permiso.dto';
+@ApiTags('Permisos')
 @Controller('permisos')
 export class PermisosController {
   constructor(private readonly permisosService: PermisosService) {}
-
+  @ApiBody({ type: CreatePermisoDto })
   @Post()
   create(@Body() createPermisoDto: CreatePermisoDto) {
     return this.permisosService.create(createPermisoDto);
   }
 
+  @ApiBody({ type: [GetPermisoDto] })
   @Get()
   findAll() {
     return this.permisosService.findAll();
   }
 
+  @ApiBody({ type: [GetPermisoDto] })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.permisosService.findOne(+id);
   }
 
+  @ApiBody({ type:  CreatePermisoDto })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePermisoDto: UpdatePermisoDto) {
     return this.permisosService.update(+id, updatePermisoDto);
