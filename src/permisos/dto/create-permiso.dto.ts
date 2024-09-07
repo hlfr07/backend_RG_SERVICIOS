7f41bc0f3f1ee70a072ddbbe39b4feaf75c78172
lolijorddy <lolijorddy@gmail.com>
2024-09-07 11:48:56 -0500

    se creo la rest api permisos

--- src/permisos/dto/create-permiso.dto.ts
+++ src/permisos/dto/create-permiso.dto.ts
@@ -1,1 +1,34 @@
-export class CreatePermisoDto {}
+import { Transform } from "class-transformer";
+import { IsNotEmpty, IsString, MaxLength, MinLength} from "class-validator";
+
+export class CreatePermisoDto {
+    @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
+    @IsNotEmpty({ message: 'El permiso get no puede estar vacío' })
+    @IsString({ message: 'El permiso get debe ser un texto' })
+    @MaxLength(10, { message: 'El permiso get debe tener menos de 50 caracteres' })
+    @MinLength(3, { message: 'El permiso get debe tener más de 3 caracteres' })
+    get: string;
+
+    @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
+    @IsNotEmpty({ message: 'El permiso post no puede estar vacío' })
+    @IsString({ message: 'El permiso post debe ser un texto' })
+    @MaxLength(10, { message: 'El permiso post debe tener menos de 50 caracteres' })
+    @MinLength(3, { message: 'El permiso post debe tener más de 3 caracteres' })
+    post: string;
+
+    @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
+    @IsNotEmpty({ message: 'El permiso put no puede estar vacío' })
+    @IsString({ message: 'El permiso put debe ser un texto' })
+    @MaxLength(10, { message: 'El permiso put debe tener menos de 50 caracteres' })
+    @MinLength(3, { message: 'El permiso put debe tener más de 3 caracteres' })
+    put: string;
+
+    @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
+    @IsNotEmpty({ message: 'El permiso delete no puede estar vacío' })
+    @IsString({ message: 'El permiso delete debe ser un texto' })
+    @MaxLength(10, { message: 'El permiso delete debe tener menos de 50 caracteres' })
+    @MinLength(3, { message: 'El permiso delete debe tener más de 3 caracteres' })
+    delete: string;
+
+    
+}
