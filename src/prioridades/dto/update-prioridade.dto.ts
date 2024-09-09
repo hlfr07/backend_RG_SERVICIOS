78054173a9fbd35490a6fd3494a6f44c66bba200
hlfr07 <hf46.78@gmail.com>
2024-09-09 12:33:45 -0500

    se elimino la tabla prioridades, es mejor usarlo como campo en la tabla perfiles

--- src/prioridades/dto/update-prioridade.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePrioridadeDto } from './create-prioridade.dto';
-
-export class UpdatePrioridadeDto extends PartialType(CreatePrioridadeDto) {}
